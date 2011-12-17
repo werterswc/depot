e3f34f7a2cd6609f08ebfaebe6f4e977b3ee0426
Werter <werter@MacBook-Pro-Werter.local>
2011-12-18 01:03:26 +0300

    Второй коммит

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
+# encoding: utf-8
 class Product < ActiveRecord::Base
+  validates :title, :description, :image_url, presence: true
+  validates :title, uniqueness: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :image_url, allow_blank: true, format: { 
+    with: %r{\.(gif|jpg|png)$}i, 
+    message: 'формат картинок только GIF, JPG или PNG.'
+  }
 end
