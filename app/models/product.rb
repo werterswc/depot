602d3cb0f9da3aef0b6aad79588e41a1e60cd41f
Werter <werter@MacBook-Pro-Werter.local>
2011-12-21 21:18:58 +0300

    Chapter 11. Task F: Add a Dash of Ajax

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 # encoding: utf-8
 class Product < ActiveRecord::Base
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+  
   validates :title, :description, :image_url, presence: true
   validates :title, uniqueness: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
@@ -7,4 +10,15 @@ class Product < ActiveRecord::Base
     with: %r{\.(gif|jpg|png)$}i, 
     message: 'формат картинок только GIF, JPG или PNG.'
   }
+  
+  private
+   # ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'line Items present')
+        return false
+      end
+    end  
 end
